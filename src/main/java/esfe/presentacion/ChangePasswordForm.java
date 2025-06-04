d34b8a9176ff47d09d05bdfcc9299dc65099b4fd
Marvin antonio Barrera Trigueros <marvin022021@outlook.es>
2025-06-03 19:46:53 -0600

    diseño del formulario de cambiar password

--- src/main/java/esfe/presentacion/ChangePasswordForm.java
+++ src/main/java/esfe/presentacion/ChangePasswordForm.java
@@ -3,4 +3,8 @@ package esfe.presentacion;
 import javax.swing.*;
 
 public class ChangePasswordForm extends JDialog {
+    private JPanel mainPanel;
+    private JTextField txtEmail;
+    private JPasswordField txtPassword;
+    private JButton btnChangePassword;
 }
