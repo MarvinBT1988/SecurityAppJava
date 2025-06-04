14c8034bb5a8971891b869c2104d5dfff9f60ad1
Marvin antonio Barrera Trigueros <marvin022021@outlook.es>
2025-06-03 20:12:11 -0600

    diseño del formulario de lectura de usuarios

--- src/main/java/esfe/presentacion/UserReadingForm.java
+++ src/main/java/esfe/presentacion/UserReadingForm.java
@@ -3,4 +3,10 @@ package esfe.presentacion;
 import javax.swing.*;
 
 public class UserReadingForm extends JDialog {
+    private JPanel mainPanel;
+    private JTextField txtName;
+    private JButton btnCreate;
+    private JTable tableUsers;
+    private JButton btnUpdate;
+    private JButton btnDelete;
 }
