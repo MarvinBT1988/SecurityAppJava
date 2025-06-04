a1b381f731b423dbb60b9af8cf0115f241e0ec4c
Marvin antonio Barrera Trigueros <marvin022021@outlook.es>
2025-06-03 21:05:46 -0600

    codificacion del formulario de iniciar session

--- src/main/java/esfe/presentacion/MainForm.java
+++ src/main/java/esfe/presentacion/MainForm.java
@@ -1,6 +1,17 @@
 package esfe.presentacion;
 
+import esfe.dominio.User;
+
 import javax.swing.*;
 
 public class MainForm extends JFrame {
+    private User userAutenticate;
+
+    public User getUserAutenticate() {
+        return userAutenticate;
+    }
+
+    public void setUserAutenticate(User userAutenticate) {
+        this.userAutenticate = userAutenticate;
+    }
 }
