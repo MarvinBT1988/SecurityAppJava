d4ca75be1ea386acc7fd30fef8b4f3a89f1015cc
Marvin antonio Barrera Trigueros <marvin022021@outlook.es>
2025-06-03 20:34:49 -0600

    diseño del formulario de escritura de usuario

--- src/main/java/esfe/presentacion/UserWriteForm.java
+++ src/main/java/esfe/presentacion/UserWriteForm.java
@@ -3,4 +3,11 @@ package esfe.presentacion;
 import javax.swing.*;
 
 public class UserWriteForm extends JDialog {
+    private JPanel mainPanel;
+    private JTextField txtName;
+    private JTextField txtEmail;
+    private JPasswordField txtPassword;
+    private JComboBox cbStatus;
+    private JButton btnOk;
+    private JButton btnCancel;
 }
