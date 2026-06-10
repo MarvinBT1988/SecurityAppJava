a4f3ab2441fb5ad4a417e2f8f377bf9004abaf56
Marvin antonio Barrera Trigueros <marvin022021@outlook.es>
2026-06-10 11:57:41 -0600

    Marvin rama2: cambios x

--- src/main/java/esfe/persistencia/ConnectionManager.java
+++ src/main/java/esfe/persistencia/ConnectionManager.java
@@ -26,7 +26,7 @@ public class ConnectionManager {
             "encrypt=true; " +
             "database=SecurityDB2025; " +
             "trustServerCertificate=true;" +
-            "user=java2025;" +
+            "user=java20251;" +
             "password=12345";
 
     /**
